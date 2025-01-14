0d5474bf9a218da49b284cbf02c4daf612d16912
Sjoerd473 <sjoerdbrussee@hotmail.com>
2025-01-14 14:13:08 +0100

    finished cv component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,173 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+// A section to add general information like name, email and phone number.
+// A section to add your educational experience (school name, title of study and date of study)
+// A section to add practical experience (company name, position title, main responsibilities of your jobs, date from and until when you worked for that company)
+
+// Be sure to include an edit and submit button for each section or for the whole CV. The submit button should submit your form and display the value of your input fields in HTML elements. The edit button should add back (display) the input fields, with the previously displayed information as values. In those input fields, you should be able to edit and resubmit the content. You’re going to make heavy use of state and props, so make sure you understood those concepts.
+function Button({ text = "Click me", isSubmitted }) {
+  return <button disabled={isSubmitted === "true"}>{text}</button>;
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+  setStatus("sent");
+}
+
+function Form({ dataType, formType = "line" }) {
+  const [status, setStatus] = useState("typing");
+  const [type, setType] = useState("line");
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState(null);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setStatus("sent");
+  }
+  function handleChange(e) {
+    e.preventDefault();
+    setStatus("typing");
+  }
+
+  function handleTextAreaChange(e) {
+    e.preventDefault();
+    setMessage(e.target.value);
+  }
+
+  if (status === "sent") {
+    return (
+      <div>
+        <h2>{dataType}</h2>
+        <p>{message}</p>
+        <Button text="Edit" onClick={handleChange} />
+      </div>
+    );
+  }
 
   return (
+    <form>
+      <label>
+        {dataType + ": "}
+        <input
+          onChange={handleTextAreaChange}
+          value={message}
+          hidden={formType === "block"}
+        />
+        <textarea hidden={formType === "line"} />
+      </label>
+      <button onClick={handleSubmit}>Submit </button>
+    </form>
+  );
+}
+
+function EditCV() {
+  const [isEditing, setIsEditing] = useState(false);
+  const [name, setName] = useState("");
+  const [city, setCity] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [school, setSchool] = useState("");
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+  const [company, setCompany] = useState("");
+  const [time, setTime] = useState("");
+  const [position, setPosition] = useState("");
+  const [resp, setResp] = useState("");
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        setIsEditing(!isEditing);
+      }}
+    >
+      <label>
+        Full name:{" "}
+        {isEditing ? (
+          <input
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+        ) : (
+          <b>{name}</b>
+        )}
+      </label>
+      <label>
+        City:{" "}
+        {isEditing ? (
+          <input
+            value={city}
+            onChange={(e) => {
+              setCity(e.target.value);
+            }}
+          />
+        ) : (
+          <b>{city}</b>
+        )}
+      </label>
+      <button type="submit">{isEditing ? "Save" : "Edit"} Profile</button>
+    </form>
+  );
+}
+function CV(){
+  const [isEditing, setIsEditing] = useState(false);
+  const fields =['Name', 'City', 'E-mail', 'Phone number', 'School', 'Title of degree', 'Time at school', 'Company', 'Time at company', 'Position held', 'Responsabilities']
+
+  function handleSubmit(e){
+    e.preventDefault()
+    setIsEditing(!isEditing)
+  }
+
+  const fieldItems = fields.map(field => 
+    <Field 
+    
+    inputType = {field}
+    status = {isEditing}
+    />
+)
+  return(
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more or less
-      </p>
+    <form
+      className="cv-form"
+      onSubmit ={handleSubmit}
+    >
+    {fieldItems}
+   
+     
+     <button type="submit">{isEditing ? "Save" : "Edit"} Profile</button>
+     </form>
     </>
   )
 }
+function Field({inputType, status}){
+  // const [isEditing, setIsEditing] = useState(false);
+  const [input, setInput] = useState("");
+  return (
+    
+      <label>
+        {inputType}:{" "}
+        {status ? (
+          <input
+            value={input}
+            onChange={(e) => {
+              setInput(e.target.value);
+            }}
+          />
+        ) : (
+          <b>{input}</b>
+        )}
+      </label>
+      
+  );
+}
 
-export default App
+ 
+
+
+
+export { CV, Form };
